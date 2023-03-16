a8f1bee0277eb08583116166e4dd04ebdbcd5257
Maikel De Werd <maikeldewerd@hotmail.com>
2023-03-16 15:41:18 +0100

    Change imports

--- src/test.jsx
+++ src/test.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function Test() {
@@ -8,5 +5,3 @@ function Test() {
     <h1>HALLLOOOOOWWW</h1>
   )
 }
-
-export default App
